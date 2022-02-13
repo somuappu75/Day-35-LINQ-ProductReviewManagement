bfd79ec8d253233263d4e7b902bcaac82377b6c6
Somanath Havinal <somusp75@gmail.com>
2022-02-13 21:13:42 +0530

    Add project files.

---
+++ LINQ-ProductReviewManagement/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LINQ_ProductReviewManagement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Product Review Management System using linq");
+        }
+    }
+}
